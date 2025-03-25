fa44abe11bb2e3aa89105eb58f803ca8b099bb7e
thanhcanhquanca <chuanhquan89@gmail.com>
2025-03-25 21:03:59 +0700

    go tay cac bieu thuc if , swith, funciton , arrow function...

--- function.js
+++ function.js
@@ -1,61 +1,31 @@
 // let city = "Ha Noi";
 // console.log(city);
-
 // city = "Ho Chi Minh";
 // console.log(city);
-
 // const count = "Viet Nam";
 // console.log(count);
-
 // các hàm bằng arrow function
-
 // const sayHello = (name) => `hello,${name}!`;
 // console.log(sayHello);
-
 // const welcome = () => "Welcom to ES6"
 // console.log(welcome());
-
-
-
-
 //  1 . Sử dụng Template Literals (*)
-
 // const name ="Jungun";
 // const age = 30;
 // const city = "New york";
 // const introduction = `My name is${name} . I am ${name} years old and i live in ${city}.`;
 // console.log(introduction);
-
-
 // const product = {name : "Laptop",
 //                   price : 1500,
 //                   brand : "Dell"};
 // const {name,price} = product;
 // console.log(name);
 // console.log(price);
-
-
-
 // 2. Sử dụng sao chep mang moi  *(array)
-
 // const fruist = ["apple" ,"banana", "orange"];
 // const newFruits = [...fruist,"mango"];
 // console.log(newFruits);
-
-
 // tinh tong trong array (*)
-
 // const sumAll = (...numbers) => numbers.reduce((acc, num) => acc + num, 0);
 // console.log(sumAll(1, 2, 3, 4));
-
 //. Tich hop tat ca cac kien thuc
-
-// const processArray = (arr) => {
-//     const doubled = arr.map((num) => num * 2);
-//     const filtered = doubled.filter((num) => num > 5);
-//     const total = filtered.reduce((acc, num) => acc + num, 0);
-//     return total;
-// };
-//
-// console.log(processArray([1, 2, 3, 4])); // Kết quả: 14
-
